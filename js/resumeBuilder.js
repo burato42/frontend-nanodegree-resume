cef5bffa8a8119077d9ac931c2bfa2aef528e25f
Timur Makhmutov <tmahmutov@gmail.com>
2015-01-11 17:39:37 +0300

    First version

---
+++ js/resumeBuilder.js
@@ -0,0 +1,80 @@
+var work = {
+	"jobs": [
+	{
+		"employer": "Sputnik",
+		"title": "SDET",
+		"location": "Moscow, Russia",
+		"dates": "2014 - Current Time",
+		"description": "Automation testing of web services"
+	},
+	{
+		"employer": "Sbertech",
+		"title": "Tester",
+		"location": "Moscow, Russia",
+		"dates": "2012 - 2014",
+		"description": "Testing of financial backoffice system"
+	}]
+};
+
+var projects = {
+	"projects": [
+	{
+		"title": "Cookies",
+		"dates": "1970 - Current Time",
+		"description": "Give me a cookie",
+		"images": "http://rack.2.mshcdn.com/media/ZgkyMDEzLzEwLzA3L2JmL0Nvb2tpZU1vbnN0LmE4NjZlLmpwZwpwCXRodW1iCTIwMHgxMTUjCmUJanBn/26cc40cf/9eb/CookieMonster.jpg"
+	}
+	]
+};
+
+var bio = {
+	"name": "Timur Makhmutov",
+	"role": "SDET",
+	"contacts": {
+		"email": "burato42@yandex.ru",
+		"mobile": "+79251111111",
+		"github": "burato42",
+		"twitter": "burato42",
+		"location": "Moscow, Russia"
+	},
+	"image": "http://vignette2.wikia.nocookie.net/muppet/images/0/08/CookieMonsterWaving.jpg/revision/latest/scale-to-width/310?cb=20120128192952",
+	"welcomeMessage": "I don't give a fuck",
+	"skills": ["QA", "Python", "JavaScript"]
+};
+
+var education = {
+	"schools": [
+	{
+		"name": "MIET",
+		"location": "Moscow",
+		"degree": "Specialist",
+		"majors": ["microelectronics"],
+		"dates": "2004 - 2012",
+		"url": "http://www.miet.ru"
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "Introduction to Computer Science and Programming",
+		"school": "edX",
+		"dates": "2012 - 2012",
+		"url": "https://www.edx.org/"
+	}, 
+	{
+		"title": "Introduction to Programming. Problem Solving with Java",
+		"school": "Udacity",
+		"dates": "2012 - 2013",
+		"url": "https://www.udacity.com"
+	}]
+};
+
+
+
+
+
+
+
+
+
+
+
